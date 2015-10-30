96b837853130344b05c61e0473bcd40fb096441d
somename <someemail>
2015-10-30 15:19:36 +0200

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -4,6 +4,27 @@ package org.unioulu.tol.sqat2015.planetExplorer;
 // ID:
 // Finish time:
 public class PlanetExplorer {
+	
+	private int xSize;
+	private int ySize;
+	
+	
+	public int getxSize() {
+		return xSize;
+	}
+
+	public void setxSize(int xSize) {
+		this.xSize = xSize;
+	}
+
+	public int getySize() {
+		return ySize;
+	}
+
+	public void setySize(int ySize) {
+		this.ySize = ySize;
+	}
+
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
