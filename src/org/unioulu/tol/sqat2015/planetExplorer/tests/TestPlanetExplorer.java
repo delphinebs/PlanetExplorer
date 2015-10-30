d95057c684ddee406ccb5860fe890ab78d26d537
somename <someemail>
2015-10-30 15:28:43 +0200

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -9,7 +9,7 @@ public class TestPlanetExplorer {
 
 	@Test
 	public void testCreatePlanetSize() {
-		PlanetExplorer planet = new PlanetExplorer(100, 100, null);
+		PlanetExplorer planet = new PlanetExplorer(100, 100, "(obs1_x,obs1_y)");
 		assertEquals(planet.getxSize(),100);
 		assertEquals(planet.getySize(),100);
 		assertEquals(planet.getObstacles(),null);
