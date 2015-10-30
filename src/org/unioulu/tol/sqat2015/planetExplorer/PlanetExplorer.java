d272351e1ab1df84eccff44737324e69890291cc
somename <someemail>
2015-10-30 17:16:56 +0200

    project finished

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -1,7 +1,7 @@
 package org.unioulu.tol.sqat2015.planetExplorer;
 
 // Before submitting write your ID and finish time here. Your ID is written on project description sheets.
-// ID: 2479820
+// ID: 140
 // Finish time: 17.16
 public class PlanetExplorer {
 	
