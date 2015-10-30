02b259cdeb97ad80d711c9966cfc381030440536
somename <someemail>
2015-10-30 16:51:00 +0200

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -89,6 +89,7 @@ public class PlanetExplorer {
 		
 		switch (this.getFace()){
 		case "N" : this.setyPosition(getyPosition()+1);
+		case "S" : this.setyPosition(getyPosition()-1);
 		}
 	}
 	public void movingBackward(){
